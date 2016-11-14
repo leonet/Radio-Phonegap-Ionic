25c5aca0c2fccf5ad2844c21e124004841821d02
Leonardo Cadavid <leonet.gnu@gmail.com>
2016-11-14 11:50:17 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -257,8 +257,8 @@
 
   app.controller('radioController', function($scope, $sce, ngAudio){
     
-    $scope.radioHost = 'http://192.99.8.192'; // Replace this with your own radio stream URL
-    $scope.radioPort = '3536'; // Replace this with the port of your Radio Stream
+    $scope.radioHost = 'http://62.210.209.179'; // Replace this with your own radio stream URL
+    $scope.radioPort = '8093'; // Replace this with the port of your Radio Stream
     $scope.lastFMKey = 'ab68e9a71c1bb15efaa9c706b646dee4';
     $scope.lastFM = 'http://ws.audioscrobbler.com/2.0/?method=track.search&format=json&limit=1&api_key='+$scope.lastFMKey+'&track=';
 
